23d5e9ed7945eb70f3f7ceb17cb70e045caafaef
toroochi <kanon.0982@gmail.com>
2024-01-14 08:19:49 +0900

    add: loginform

--- tsumugu/src/App.js
+++ tsumugu/src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/style.css';
 
 function App() {
   return (
     <body>
-      <div className="App">
-        <div className="container">
         <h1>
+          <p>　みんなと、小説を紡ぐ</p>
           <span>Re:Tale</span> 
         </h1>
-        <p>あああああああああああああ</p>
+        <div className="right">
+        <div class="content">
+          <div class="text">Login Form</div>
+          <form action="#">
+            <div class="field">
+              <span class="fas fa-user"></span>
+              <input type="text" required></input>
+              <label>Email</label>
+            </div>
+            <div class="field">
+              <span class="fas fa-lock"></span>
+              <input type="password"></input>
+              <label>Password</label>
+            </div>
+            <div class="forgot-pass"><a href="#">Forgot Password?</a></div>
+            <button><p>Sign in</p></button>
+            <div class="signup">Not a member?
+              <a href="#">signup now</a>
+            </div>
+          </form>
+          </div>
         </div>
-      </div>
     </body>
   );
 }
